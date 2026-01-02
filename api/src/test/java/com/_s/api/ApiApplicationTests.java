9d1f13b7105d7379e37602ef53e7cecc18da942d
samuelmaia1 <smaia190500@gmail.com>
2026-01-02 17:14:15 -0300

    Create Project

---
+++ api/src/test/java/com/_s/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com._s.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
