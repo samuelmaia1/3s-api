71c516fa7b0d3e71b34272444f86ed323f285c92
samuelmaia-appvisto <samuel.maia@appvisto.com>
2026-02-04 10:15:26 -0300

    update cors config and application.properties

--- api/src/main/java/com/_s/api/infra/config/ApiWebConfig.java
+++ api/src/main/java/com/_s/api/infra/config/ApiWebConfig.java
@@ -1,16 +1,19 @@
 package com._s.api.infra.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApiWebConfig implements WebMvcConfigurer {
+    @Value("${api.web.config.allowed-origin}")
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
