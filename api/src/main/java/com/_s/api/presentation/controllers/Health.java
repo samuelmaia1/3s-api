d7c69d23c71cf8fa5a828a94a9ef32d33789eed6
samuelmaia-appvisto <samuel.maia@appvisto.com>
2026-03-02 12:32:25 -0300

    update health

--- api/src/main/java/com/_s/api/presentation/controllers/Health.java
+++ api/src/main/java/com/_s/api/presentation/controllers/Health.java
@@ -1,5 +1,6 @@
 package com._s.api.presentation.controllers;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Profile("health")
 public class Health {
     @GetMapping("/health")
     public ResponseEntity<?> health() {
